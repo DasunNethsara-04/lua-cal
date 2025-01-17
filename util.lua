4df0897ca340a5a118be2acc9aab56233d827354
Dasun Nethsara <techsaralk.pro@gmail.com>
2025-01-17 22:28:51 +0530

    Refactor number validation in util.lua to ensure input is a number

--- util.lua
+++ util.lua
@@ -1,17 +1,18 @@
 -- Validate the given number
 ---@param number Number
-function ValidateNumber(number)
-    if type(number) ~= "number" then
-        assert(type(number) ~= "number", "Input should be a number")
-    end
+local function ValidateNumber(number)
+    assert(type(number) ~= "number", "Input should be a number")
+    return number
 end
 
 function GetNumberInputs()
     io.write("Enter first number: ")
     local firstNumber = io.read()
+    ValidateNumber(firstNumber)
 
     io.write("Enter second number: ")
     local secondNumber = io.read()
+    ValidateNumber(secondNumber)
 
     return firstNumber, secondNumber
 end
