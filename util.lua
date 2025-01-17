32e65b71a8e1b1f559d69022f5d1a752c86a37bb
Dasun Nethsara <techsaralk.pro@gmail.com>
2025-01-17 22:23:39 +0530

    Add number validation function to util.lua

--- util.lua
+++ util.lua
@@ -1,3 +1,11 @@
+-- Validate the given number
+---@param number Number
+function ValidateNumber(number)
+    if type(number) ~= "number" then
+        assert(type(number) ~= "number", "Input should be a number")
+    end
+end
+
 function GetNumberInputs()
     io.write("Enter first number: ")
     local firstNumber = io.read()
